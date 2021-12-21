6ee81ed9cf025cfdc04b65894e2c5d173aac8bc2
Gaku Nakagawa <48303198+gnakagaw@users.noreply.github.com>
2021-12-21 11:34:51 +0900

    Update my_program.rb

--- my_program.rb
+++ my_program.rb
@@ -3,5 +3,5 @@ AWS_SECRET_ACCESS_KEY='wJalrXUtnFEMI/K7MDENG/cPaRfiCYEXAMPLEKEY'.freeze
 
 puts(AWS_ACCESS_KEY_ID)
 puts(AWS_SECRET_ACCESS_KEY)
-puts("10 is #{10.to_i}")
+puts("10 is #{'10'.to_i}")
 puts("zeebra is not zebra!")
