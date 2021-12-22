df0278bf334420408bf5797e6223275608053438
Gaku Nakagawa <48303198+gnakagaw@users.noreply.github.com>
2021-12-22 12:19:10 +0900

    Create program.rb

---
+++ program.rb
@@ -0,0 +1,7 @@
+AWS_ACCESS_KEY_ID='AKIAIAKFODNN7EXAMPLE'.freeze
+AWS_SECRET_ACCESS_KEY='wJalrXUtnFEMI/K7MDENG/cPaRfiCYEXAMPLEKEY'.freeze
+
+puts(AWS_ACCESS_KEY_ID)
+puts(AWS_SECRET_ACCESS_KEY)
+puts("10 is #{'10'.to_i}")
+puts("zeebra is not zebra!")
